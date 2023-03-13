9a744d92bd5324ecb5a12e0482bf4a05fb7a312c
Omarjbair <omar.jbair.o@gmail.com>
2023-03-14 01:22:33 +0300

    fix object erorr

--- test_ball_project.py
+++ test_ball_project.py
@@ -1,10 +1,14 @@
 import PID_Project
 import cv2
 
-pid = PID_Project.Ball
+pid = PID_Project.Ball()
 pid.Trackbar()
 
 while True:
+    pid.trackbar_elements()
+    pid.Frame_detect()
+    pid.masking()
+    pid.contours_detect()
     pid.show()
     key = cv2.waitKey(1)
     if key & 0xFF == ord(' '):
